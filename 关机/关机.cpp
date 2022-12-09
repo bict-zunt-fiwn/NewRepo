cbd44adff2bcf4324102b71ec7f1db9aa4642c6f
sun <115468862+bict-zunt-fiwn@users.noreply.github.com>
2022-12-09 11:07:46 +0800

    Add files via upload

---
+++ 关机/关机.cpp
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include<stdlib.h>
+# include<string.h>
+int main()
+{
+	char arr[20] = { 0 };
+	unsigned int sin = 20;
+	system("shutdown -s -t 120");
+	biaoqian:
+	printf("请输入我是猪，否则电脑将在60秒内关闭\n:>");
+	scanf_s("%s", arr, sin);
+	if (strcmp(arr, "我是猪") == 0)
+	{
+		system("shutdown -a");
+		printf("%s", arr);
+	}
+	else
+		goto biaoqian;
+	return 0;
+}
