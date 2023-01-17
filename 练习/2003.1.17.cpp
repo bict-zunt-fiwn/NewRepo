fc3fc060217455aaeb941de61db3bc526bcef7b5
sun <115468862+bict-zunt-fiwn@users.noreply.github.com>
2023-01-17 22:48:59 +0800

    Add files via upload

---
+++ 练习/2003.1.17.cpp
@@ -0,0 +1,151 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+# include<stdio.h>
+# include<stdlib.h>
+# include<windows.h>
+//int check()
+//{
+//	int a = 1;
+// //返回1为小端存储
+// //返回0为大端存储
+//	return *(char*)&a;
+//}
+//int main()
+//{
+//	if (check())
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char a = -1;
+//	signed char b= -1;
+//	unsigned char c = -1;
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a;
+//	a = 0;
+//	int b;
+//	b = 2;
+//	system("color 06");
+//	while (a >=0 || b > 0)
+//	{
+//		system("cls");
+//		printf("%d:%02d", b,a);
+//		Sleep(50);
+//		if (a == 0 && b != 0)
+//		{
+//			b--;
+//			a = 60;
+//		}
+//		a--;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int b = 1;
+//	while (b<=a)
+//	{
+//		int c = b;
+//		while(c)
+//		{
+//			printf("%d ", b);
+//			c--;
+//		}
+//		printf("\n");
+//		b++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int b = 1;
+//	while (b<=a)
+//	{
+//		int c = b;
+//		while (c)
+//		{
+//			static int d = 1;
+//			printf("%02d ",d);
+//			d++;
+//			c--;
+//		}
+//		printf("\n");
+//		b++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	a = 0;
+//	while(a <= 10)
+//	{
+//		system("cls");
+//		b = 1;
+//		while (b <= a)
+//		{
+//			printf(" ");
+//			b++;
+//		}
+//		printf("H");
+//		Sleep(50);
+//		a++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	a = 0;
+//	while (a <= 100)
+//	{
+//		system("cls");
+//		b = 1;
+//		while (b <= a)
+//		{
+//			printf(" ");
+//			b++;
+//		}
+//		printf(" O\n");
+//		
+//		b = 1;
+//		while (b <= a)
+//		{
+//			printf(" ");
+//			b++;
+//		}
+//		printf("<H>\n");
+//
+//		b = 1;
+//		while (b <= a)
+//		{
+//			printf(" ");
+//			b++;
+//		}
+//		printf("I I\n");
+//
+//		Sleep(50);
+//		a++;
+//	}
+//	return 0;
+//}
+int main()
+{
+	const char* p = "abcde";
+
+	return 0;
+}
